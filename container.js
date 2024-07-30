7537e2313737500920cfbe27c4d82d2b74afc2d2
shubham Rawat <rawatshubam269@gmail.com>
2024-07-30 21:07:59 +0530

    sticky navbar + scroll to top issue fix

--- container.js
+++ container.js
@@ -1,34 +1,20 @@
 const p_btn = document.querySelectorAll(".spcl_class");
 const images = document.querySelectorAll(".img_overlay");
-
-// portfolio logic section start here
-$(".portfolio_buttons").click(function (e) {
-  const btn_clicked = e.target;
-  // console.log(btn_clicked);
-  p_btn.forEach((ele) => ele.classList.remove("btn_active"));
-  btn_clicked.classList.add("btn_active");
-
-  const btn_number = btn_clicked.getAttribute("data-btn-num");
-  // console.log(btn_number);
-
-  const img_active = document.querySelectorAll(`.p-btn--${btn_number}`);
-
-  images.forEach((curElem) => curElem.classList.add("dnone"));
-  img_active.forEach((curElem) => curElem.classList.remove("dnone"));
-});
+const header = document.querySelector(".header");
+const heroSection = document.getElementById("hero-section")
 
 // sticky navbar logic
 
-const hero = document.querySelector(".section_hero");
+const hero = document.querySelector(".section-hero");
 
 const observer = new IntersectionObserver(
   (enteries) => {
     const ent = enteries[0];
     // console.log(ent);
     if (ent.isIntersecting == false) {
-      document.body.classList.add("sticky");
+      header.classList.add("sticky");
     } else {
-      document.body.classList.remove("sticky");
+      header.classList.remove("sticky");
     }
   },
   { root: null, threshold: 0 }
@@ -37,32 +23,34 @@ const observer = new IntersectionObserver(
 observer.observe(hero);
 
 // scroll to top functionality start here
-const header = document.querySelector(".header");
+
 const footerDiv = document.querySelector(".section-footer");
 let scrollTotop = document.createElement("div");
 scrollTotop.classList.add("scrollTop-style");
+
 
 scrollTotop.innerHTML = `
 <i class="fa-solid fa-arrow-up scroll-top fa-2x"></i>`;
 footerDiv.after(scrollTotop);
 
+
 scrollTotop.addEventListener("click", function () {
-  header.scrollIntoView({ behavior: "smooth" });
+  heroSection.scrollIntoView({ behavior: "smooth" });
 });
 
 // hamburger logic start here
 
-const icon1 = document.getElementById("ham-menu");
-const icon2 = document.getElementById("cross-menu");
-const menu = document.querySelector(".menu-section");
+// const icon1 = document.getElementById("ham-menu");
+// const icon2 = document.getElementById("cross-menu");
+// const menu = document.querySelector(".menu-section");
 
-icon1.addEventListener("click", function () {
-  menu.classList.add("active");
-  icon1.style.display = "none";
-  icon2.style.display = "block";
-});
-icon2.addEventListener("click", function () {
-  menu.classList.remove("active");
-  icon2.style.display = "none";
-  icon1.style.display = "block";
-});
+// icon1.addEventListener("click", function () {
+//   menu.classList.add("active");
+//   icon1.style.display = "none";
+//   icon2.style.display = "block";
+// });
+// icon2.addEventListener("click", function () {
+//   menu.classList.remove("active");
+//   icon2.style.display = "none";
+//   icon1.style.display = "block";
+// });
